4553852e2af4eb4dfc9924c69e2fcfb1056a2988
Will Dietz <w@wdtz.org>
2014-07-24 11:54:18 -0500

    iperf.sh: Add warning about being potentially unreliable.
    Sometimes it hangs trying to run the server, have not debugged
    yet so not sure if some bug in our logic or the automation.

--- docker/ipc-test/iperf.sh
+++ docker/ipc-test/iperf.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "This test seems to be unreliable, presently, when automated."
+echo "Not sure why, will investigate, but it's a known issue :)."
+
 # Create base ipc-hook container
 . ./build.sh
 
