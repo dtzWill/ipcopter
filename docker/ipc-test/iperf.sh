e945fcf072b6eb8d4b22b142e0651817b79ddb2c
Will Dietz <w@wdtz.org>
2014-07-25 17:15:07 -0500

    iperf.sh: Give prompt to abort unreliable test...

--- docker/ipc-test/iperf.sh
+++ docker/ipc-test/iperf.sh
@@ -2,6 +2,8 @@
 
 echo "This test seems to be unreliable, presently, when automated."
 echo "Not sure why, will investigate, but it's a known issue :)."
+echo "Press control-c to abort now, or any regular key to proceed anyway"
+read
 
 # Create base ipc-hook container
 . ./build.sh
