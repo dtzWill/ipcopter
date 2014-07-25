cef3a9f6f9277e72886e720608e120e8935b37ba
Will Dietz <w@wdtz.org>
2014-07-25 16:25:39 -0500

    Inter-container TCP optimization working! :D
    * Run ipcd in dedicated container (because we can :P)
    * Run server/client in normal ipc-able containers
      * Contains libipc.so and /etc/ld.so.preload to ensure it's used
    * When starting server/client, give docker single extra flag
      * This flag bind-mounts the UDS for talking to ipcd
        into each of the participating containers so they
        all talk to the same 'central manager'.
    
    That's it!
    
    During optimization phase, ipcd normally creates
    a connected pair of sockets and passes each
    process one end of this pair for use as their
    direct fast transport.  This open-fd passing
    is done via UDS, so since we're able to establish
    our UDS into the containers this mechanism
    still works!
    
    This is cool because it gives the containers
    a means for talking directly despite there
    being no existing way for them to open
    connected sockets in this way themselves!

---
+++ docker/inter-nc/inter-nc.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+# XXX: Assumes will/ipc-hook already exists
+
+function status() {
+  echo
+  echo
+  echo "--------------------------------------------------------------------------------"
+  echo "$@"
+  echo "--------------------------------------------------------------------------------"
+}
+
+function update() {
+  echo "  * $@"
+}
+
+
+
+status "Generating containers"
+update "Generating IPCD container..."
+docker build -q -t $USER/ipcd ipcd > /dev/null
+
+update "Generating server container..."
+docker build -q -t $USER/nc-server nc-server > /dev/null
+update "Generating client container..."
+docker build -q -t $USER/nc-client nc-client > /dev/null
+
+status "Starting containers"
+
+update "Starting IPCD container..."
+docker run -d --name ipcd --volume /tmp $USER/ipcd > /dev/null
+sleep 1
+
+update "Starting server with ipcd socket mounted into it..."
+docker run -d --name nc-server --volumes-from ipcd $USER/nc-server > /dev/null
+sleep 1
+update "Starting client container linked to server, also with ipcd socket mounted..."
+docker run -d --name nc-client --volumes-from ipcd --link nc-server:server $USER/nc-client > /dev/null
+
+status "Monitor experiment"
+docker attach nc-server
+update "Transfer complete."
+
+status "Cleanup! Please be patient..."
+update "Waiting for cient to ensure it's done:"
+docker wait nc-client >/dev/null
+
+update "Stopping server and client containers..."
+docker stop -t=1 nc-server nc-client > /dev/null
+update "And removing their disk images..."
+docker rm nc-server nc-client > /dev/null
+update "Stopping ipcd container..."
+docker stop -t=1 ipcd > /dev/null
+update "And removing its disk image as well..."
+docker rm ipcd > /dev/null
+
+echo
+status "** Done! ** "
+echo
