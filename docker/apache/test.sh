01e447842cdccaab25d2f452729a00eb8f884478
Will Dietz <w@wdtz.org>
2014-07-28 22:07:34 -0500

    docker: Commit wget testing script for apache2.

---
+++ docker/apache/test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+. ./build.sh
+
+TEST_CMD="
+service apache2 start
+wget http://localhost/100M.bin -O /dev/null
+wget http://localhost/100M.bin -O /dev/null
+wget http://localhost/100M.bin -O /dev/null
+wget http://localhost/10M.bin -O /dev/null
+wget http://localhost/10M.bin -O /dev/null
+wget http://localhost/10M.bin -O /dev/null
+wget http://localhost/1M.bin -O /dev/null
+wget http://localhost/1M.bin -O /dev/null
+wget http://localhost/1M.bin -O /dev/null
+service apache2 stop"
+
+. ../test.inc
+
+
