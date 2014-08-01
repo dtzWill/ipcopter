5454bd6962369f253f22747e3ee1f322c1587992
Will Dietz <w@wdtz.org>
2014-07-31 22:49:54 -0500

    docker/memcached: Multithreaded test since we now are able to!! :D
    CRC32 of send/recv is enough to make multithreaded memcached
    test work properly (it doesn't with previous ENDPOINT_KLUDGE method).
    
    Still weak, but should work on more programs/benchmarks now!

--- docker/memcached/test.sh
+++ docker/memcached/test.sh
@@ -6,7 +6,7 @@ TEST_CMD="
 service memcached start
 sleep 1
 cd memcachetest
-./memcachetest -h 127.0.0.1 -t 1 -P 33 -i 10000 -c 200000 -l -T 10
+./memcachetest -h 127.0.0.1 -t 30 -W 20 -P 33 -i 10000 -c 200000 -l -T 10
 service memcached stop
 sleep 1"
 
