489dcce227753043b8f41b9f632d3827468d0aff
Will Dietz <w@wdtz.org>
2014-08-05 21:38:45 -0500

    docker: Remove some unhelpful lmbench tests, print output file too.

--- docker/lmbench/test.sh
+++ docker/lmbench/test.sh
@@ -12,6 +12,9 @@ ln -s /CONFIG /usr/lib/lmbench/bin/x86_64-linux-gnu/CONFIG.\$HOSTNAME
 mkdir -p \$CONFIG_DIR
 ln -s /CONFIG \$CONFIG
 lmbench-run
+echo '**************************'
+cat /lmbench.log
+echo '**************************'
 cat \$RESULTS_DIR/\${HOSTNAME}.0
 "
 
