c423608ed8ef506ac7db93947096575f119e9f2f
Will Dietz <w@wdtz.org>
2014-08-05 21:11:27 -0500

    docker: Add scripts for lmbench, including CONFIG for presto.

---
+++ docker/lmbench/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+NAME=lmbench
+
+. ../build.inc
---
+++ docker/lmbench/test.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+. ./build.sh
+
+TEST_CMD="
+DATA_DIR=/var/lib/lmbench
+CONFIG_DIR=\$DATA_DIR/config
+CONFIG=\$CONFIG_DIR/CONFIG.\$HOSTNAME
+RESULTS_DIR=\$DATA_DIR/results/x86_64-linux-gnu
+
+ln -s /CONFIG /usr/lib/lmbench/bin/x86_64-linux-gnu/CONFIG.\$HOSTNAME
+mkdir -p \$CONFIG_DIR
+ln -s /CONFIG \$CONFIG
+lmbench-run
+cat \$RESULTS_DIR/\${HOSTNAME}.0
+"
+
+. ../test.inc
+
+
