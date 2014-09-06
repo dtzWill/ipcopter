13f45a7d7db145720b3036f4d1d3a474bfb5e222
Will Dietz <w@wdtz.org>
2014-09-05 20:02:30 -0500

    docker: Update zeromq test script to better match that done for eval.

--- docker/zeromq/test.sh
+++ docker/zeromq/test.sh
@@ -4,20 +4,28 @@
 
 TEST_CMD="
 cd /zeromq-4.0.4/perf
-./local_thr tcp://lo:5555 1 1000000 &
-./remote_thr tcp://localhost:5555 1 1000000
+./local_thr tcp://lo:5555 1 10000000 &
+./remote_thr tcp://localhost:5555 1 10000000
 sleep 1
 wait
-./local_thr tcp://lo:5555 10 100000 &
-./remote_thr tcp://localhost:5555 10 100000
+./local_thr tcp://lo:5555 10 1000000 &
+./remote_thr tcp://localhost:5555 10 1000000
 sleep 1
 wait
 ./local_thr tcp://lo:5555 100 100000 &
 ./remote_thr tcp://localhost:5555 100 100000
 sleep 1
 wait
-./local_thr tcp://lo:5555 1000 100000 &
-./remote_thr tcp://localhost:5555 1000 100000
+./local_thr tcp://lo:5555 1000 10000 &
+./remote_thr tcp://localhost:5555 1000 10000
+wait
+sleep 1
+./local_thr tcp://lo:5555 10000 1000 &
+./remote_thr tcp://localhost:5555 10000 1000
+wait
+sleep 1
+./local_thr tcp://lo:5555 100000 100 &
+./remote_thr tcp://localhost:5555 100000 100
 wait
 "
 
