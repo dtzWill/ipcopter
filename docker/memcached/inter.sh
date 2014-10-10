c4438d624f2db5544564e9127d111a29431accda
Will Dietz <w@wdtz.org>
2014-10-10 15:43:30 -0500

    docker: Update memcache to nuke ipcd from baseline.

--- docker/memcached/inter.sh
+++ docker/memcached/inter.sh
@@ -33,6 +33,16 @@ docker build -q -t $USER/mem-server server > build.server.log
 update "Generating client container..."
 docker build -q -t $USER/mem-client client > build.client.log
 
+update "Generating versions without any of our tech..."
+DISABLE_CMD="rm /etc/ld.so.preload /bin/ipcd /lib/libipc.so"
+DISABLE_CMD="$DISABLE_CMD && rm -rf /tmp/ipcd*"
+for t in {server,client}; do
+  mkdir -p tmp
+  echo "RUN $DISABLE_CMD" | cat $t/Dockerfile - > tmp/Dockerfile
+  docker build -q -t $USER/$t-clean tmp
+done
+rm -rf ./tmp
+
 status "Starting containers for IPCD experiment"
 
 update "Starting IPCD container..."
@@ -63,23 +73,23 @@ docker rm ipcd > /dev/null
 
 status "Starting containers for non-IPCD experiment:"
 update "Starting server container..."
-docker run -d --name mem-server $USER/mem-server > /dev/null
+docker run -d --name mem-server-clean $USER/mem-server-clean > /dev/null
 sleep 1
 update "Starting client container linked to server..."
-docker run -d --name mem-client --link mem-server:server $USER/mem-client > /dev/null
+docker run -d --name mem-client-clean --link mem-server-clean:server $USER/mem-client-clean > /dev/null
 
 status "Monitor experiment (ipcd *NOT* enabled)"
-docker attach mem-client
+docker attach mem-client-clean
 update "Transfer complete."
 
 status "Cleanup! Please be patient..."
 update "Waiting for cient to ensure it's done:"
-docker wait mem-client >/dev/null
+docker wait mem-client-clean >/dev/null
 
 update "Stopping server and client containers..."
-docker stop -t=1 mem-server mem-client > /dev/null
+docker stop -t=1 mem-server-clean mem-client-clean > /dev/null
 update "And removing their disk images..."
-docker rm mem-server mem-client > /dev/null
+docker rm mem-server-clean mem-client-clean > /dev/null
 
 echo
 status "** Done! ** "
